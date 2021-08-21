f8ac7c6bfcc202a19e71d165e546e862df7d699b
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2021-08-21 12:56:15 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -22,145 +22,181 @@ const users =
   }, {
     "name": "Amada Cerulli",
     "img": "https://www.gravatar.com/avatar/5?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=05",
     "created": "July 22, 2009"
   }, {
     "name": "Harriette Garcia",
     "img": "https://www.gravatar.com/avatar/6?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=06",
     "created": "July 29, 2018"
   }, {
     "name": "Erminia Marotz",
     "img": "https://www.gravatar.com/avatar/7?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=07",
     "created": "September 29, 2016"
   }, {
     "name": "Shanelle Parodi",
     "img": "https://www.gravatar.com/avatar/8?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=08,
     "created": "May 26, 2018"
   }, {
     "name": "Roger Leite",
     "img": "https://www.gravatar.com/avatar/9?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=09",
     "created": "August 6, 2015"
   }, {
     "name": "Latina Faulcon",
     "img": "https://www.gravatar.com/avatar/10?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=10",
     "created": "February 5, 2014"
   }, {
     "name": "Jerrie Hoekstra",
     "img": "https://www.gravatar.com/avatar/11?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=11",
     "created": "June 2, 2016"
   }, {
     "name": "Domonique Byam",
     "img": "https://www.gravatar.com/avatar/12?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=12",
     "created": "December 30, 2010"
   }, {
     "name": "Monnie Bonar",
     "img": "https://www.gravatar.com/avatar/13?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=13",
     "created": "December 20, 2018"
   }, {
     "name": "Chu Kahle",
     "img": "https://www.gravatar.com/avatar/14?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=14",
     "created": "November 17, 2017"
   }, {
     "name": "Allan Passman",
     "img": "https://www.gravatar.com/avatar/15?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=15",
     "created": "November 12, 2015"
   }, {
     "name": "Conrad Caliendo",
     "img": "https://www.gravatar.com/avatar/16?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=16",
     "created": "February 10, 2016"
   }, {
     "name": "Elma Chenier",
     "img": "https://www.gravatar.com/avatar/17?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=17",
     "created": "August 13, 2011"
   }, {
     "name": "Wendi Hirano",
     "img": "https://www.gravatar.com/avatar/18?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=18",
     "created": "July 27, 2018"
   }, {
     "name": "Loren Wordlaw",
     "img": "https://www.gravatar.com/avatar/19?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=19",
     "created": "December 20, 2014"
   }, {
     "name": "Hubert Frum",
     "img": "https://www.gravatar.com/avatar/20?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=20",
     "created": "January 21, 2013"
   }, {
     "name": "Rueben Basil",
     "img": "https://www.gravatar.com/avatar/21?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=21",
     "created": "December 2, 2013"
   }, {
     "name": "Krystyna Hardiman",
     "img": "https://www.gravatar.com/avatar/22?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=22",
     "created": "February 11, 2016"
   }, {
     "name": "Micki Murch",
     "img": "https://www.gravatar.com/avatar/23?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=23",
     "created": "December 17, 2009"
   }, {
     "name": "Allene Knight",
     "img": "https://www.gravatar.com/avatar/24?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=24",
     "created": "November 3, 2010"
   }, {
     "name": "Davis Lunsford",
     "img": "https://www.gravatar.com/avatar/25?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=45",
     "created": "October 17, 2011"
   }, {
     "name": "Elin Conte",
     "img": "https://www.gravatar.com/avatar/26?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=46",
     "created": "June 23, 2015"
   }, {
     "name": "Yasuko Hites",
     "img": "https://www.gravatar.com/avatar/27?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=47",
     "created": "August 25, 2017"
   }, {
     "name": "Gerri Pinon",
     "img": "https://www.gravatar.com/avatar/28?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=48",
     "created": "May 21, 2014"
   }, {
     "name": "Caryl Hawker",
     "img": "https://www.gravatar.com/avatar/29?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=49",
     "created": "April 13, 2018"
   }, {
     "name": "Savannah Hoard",
     "img": "https://www.gravatar.com/avatar/30?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=50",
     "created": "October 31, 2009"
   }, {
     "name": "Carolyn Knutsen",
     "img": "https://www.gravatar.com/avatar/31?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=51",
     "created": "July 16, 2015"
   }, {
     "name": "Shantay Mckissack",
     "img": "https://www.gravatar.com/avatar/32?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "July 9, 2010"
   }, {
     "name": "Vertie Pinales",
     "img": "https://www.gravatar.com/avatar/33?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "November 20, 2010"
   }, {
     "name": "Gidget Stuck",
     "img": "https://www.gravatar.com/avatar/34?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "August 17, 2017"
   }, {
     "name": "Drew Crownover",
     "img": "https://www.gravatar.com/avatar/35?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "August 30, 2017"
   }, {
     "name": "Vashti Krajewski",
     "img": "https://www.gravatar.com/avatar/36?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "January 25, 2018"
   }, {
     "name": "Candice Dike",
     "img": "https://www.gravatar.com/avatar/37?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "November 19, 2018"
   }, {
     "name": "Dorthey Buhler",
     "img": "https://www.gravatar.com/avatar/38?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "October 22, 2012"
   }, {
     "name": "Hailey Deluna",
     "img": "https://www.gravatar.com/avatar/39?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "September 13, 2012"
   }, {
     "name": "Richard Aaron",
     "img": "https://www.gravatar.com/avatar/40?d=wavatar",
+    "url": "https://via.placeholder.com/500x500/0000FF/808080?text=03",
     "created": "April 27, 2016"
   }];
